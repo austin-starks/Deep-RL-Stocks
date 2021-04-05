f357a15f17dc2506fa8009c4342863ddf1c81263
Austin Starks <austin.starks@sv.cmu.edu>
2021-04-05 14:13:16 -0400

    Finish refactor

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import logging
 import datetime
 import utils
 from pathlib import Path
+import train
 
 
 if __name__ == "__main__":
     path = os.path.dirname(Path(__file__).absolute())
 
-    # Can log with utils.log_info(args)
     logging.basicConfig(
-        filename=f'{path}/logs/{datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S")}.log',
+        filename=f'{path}/logs/{"log"}.log',
         format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
         datefmt='%Y-%m-%d:%H:%M:%S',
-        level=logging.INFO)
+        level=logging.INFO,
+        filemode="w")
+
+    train.run()
+
+    
