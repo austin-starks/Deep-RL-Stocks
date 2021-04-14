b2286e34bacbafd866f1f0af2c8e32f0d24d4164
Austin Starks <austin.starks@sv.cmu.edu>
2021-04-13 21:01:29 -0400

    Refactor things into folders

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import train
+import models.environment as env
 
 
 
 if __name__ == "__main__":
-    policy, replay_buffer = train.run(["AAPL", 'NVDA', 'MSFT'], '01-01-2011', '01-01-2015')
-    train.test(["AAPL", 'NVDA', 'MSFT'], '01-01-2016', '09-30-2018', policy, replay_buffer)
+    policy, replay_buffer = env.run(["AAPL", 'NVDA', 'MSFT'], '01-01-2011', '01-01-2015')
+    env.test(["AAPL", 'NVDA', 'MSFT'], '01-01-2016', '09-30-2018', policy, replay_buffer)
 
 
     
