33ab357606d38ab113f75f740930b2e172ffac39
Austin Starks <austin.starks@sv.cmu.edu>
2021-05-11 16:15:30 -0400

    Add back save/load

--- models/model.py
+++ models/model.py
@@ -271,6 +271,24 @@ class TD3(object):
                     self.tau * param.data + (1 - self.tau) * target_param.data
                 )
     
+    def save(self, filename):
+        torch.save(self.critic.state_dict(), filename + "_critic")
+        torch.save(self.critic_optimizer.state_dict(), filename + "_critic_optimizer")
+        torch.save(self.actor.state_dict(), filename + "_actor")
+        torch.save(self.actor_optimizer.state_dict(), filename + "_actor_optimizer")
+
+    def load(self, filename):
+        self.critic.load_state_dict(torch.load(filename + "_critic"))
+        self.critic_optimizer.load_state_dict(
+            torch.load(filename + "_critic_optimizer")
+        )
+        self.critic_target = copy.deepcopy(self.critic)
+        self.actor.load_state_dict(torch.load(filename + "_actor"))
+        self.actor_optimizer.load_state_dict(torch.load(filename + "_actor_optimizer"))
+        self.actor_target = copy.deepcopy(self.actor)
+
+
+    
 
 
 class ReplayBuffer(object):
